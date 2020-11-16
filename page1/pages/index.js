629970d183faa46505ed00d081003017d0388ffc
Butterfly <matheus.vendas.afiliado@gmail.com>
2020-11-15 21:50:08 -0300

    primeira página

--- page1/pages/index.js
+++ page1/pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Mendigo Digital</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Mendigo Digital <a href="https://www.paypal.com/donate?hosted_button_id=RPGPQ64JK7B86">Ajude Alguém!</a>
+          Mendigo Digital <a href="https://www.paypal.com/donate?hosted_button_id=RPGPQ64JK7B86">Faça o Bem Sem Olhar a Quem!</a>
         </h1>
 
         <p className={styles.description}>
@@ -20,7 +20,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Doação para compra de um computador para estudos - Clique no link abaixo &rarr;</h3>
+            <h3>Doação espontânea de caridade por boa fé - Clique Aqui &rarr;</h3>
             <a href="https://www.paypal.com/donate?hosted_button_id=RPGPQ64JK7B86">Doar pelo PayPal</a>
           </a>
          
